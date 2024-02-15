a276daeefa26da7b6f37879609e3a43bdbbe1799
Ezeh Kingsley Uchenna <79694452+ezehkingsleyuchenna@users.noreply.github.com>
2024-02-15 13:40:01 +0100

    Update Counter.jsx

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,8 +1,14 @@
 
 export default function Counter() {
     return (
-        <>
-            
-        </>
+        <div className="wrapper">
+            <h1>Counter</h1>
+            <h2>0</h2>
+            <div className="flex">
+                <button className="btn">Increment</button>
+                <button className="btn">Reset</button>
+                <button className="btn">Decrement</button>
+            </div>
+        </div>
     );
 }
